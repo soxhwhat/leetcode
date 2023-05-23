3871e95d8696a98e7f2c5a2e965038e3492b074e
soxhwhat <1246669436@qq.com>
2023-05-23 22:39:38 +0800

    docs: [递归] 1080 InsufficientNodesInRootToLeafPaths
    递归判断路径和是否满足条件，所以参数需要增加节点的值
    
    如果当前节点为空 直接返回false
    int temp = root.val + sum;
    如果当前temp无子节点即为叶子节点，返回temo >= limit
    递归判断左右节点是否符合条件，如果左右节点不符合直接删去
    结果为判断左右路径是否有一个符合条件

---
+++ leetcode/src.main.java.com.soxhwhat/leetcode/editor/cn/P1080_InsufficientNodesInRootToLeafPaths .java
@@ -0,0 +1,55 @@
+package leetcode.editor.cn;
+
+/**
+ * 根到叶路径上的不足节点
+ * insufficient-nodes-in-root-to-leaf-paths
+ * 2023-05-23 22:25:51
+ * @author soxhwhat
+ */
+class P1080_InsufficientNodesInRootToLeafPaths{
+    public static void main(String[] args) {
+        Solution solution = new P1080_InsufficientNodesInRootToLeafPaths().new Solution();
+        
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int LIMIT;
+    public TreeNode sufficientSubset(TreeNode root, int limit) {
+        LIMIT = limit;
+        boolean flag = dfs(root, 0);
+        return flag ? root : null;
+    }
+
+    private boolean dfs(TreeNode root, int sum) {
+        if (root == null) return false;
+        int temp = sum + root.val;
+        if (root.left == null && root.right == null) return  temp >= LIMIT;
+
+        boolean leftFlag= dfs(root.left, temp);
+        boolean rightFlag = dfs(root.right, temp);
+
+        if (!leftFlag) root.left = null;
+        if (!rightFlag) root.right = null;
+
+        return leftFlag || rightFlag;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
